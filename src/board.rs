748bf688d09008c91c716313d8804f3ee54af1f2
Edward Wang <edawordwang@gmail.com>
2024-02-12 00:24:52 -0500

    done

--- src/board.rs
+++ src/board.rs
@@ -1,21 +1,49 @@
+use std::fmt;
+
 const SIZE: usize = 3;
 
-#[derive(Clone, Copy)]
-enum Piece {X, O, Empty}
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum Piece {X, O, Empty}
 
-enum PlaceError {
+impl Default for Piece {
+    fn default() -> Self {
+        Piece::Empty
+    }
+}
+
+impl fmt::Display for Piece {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {
+            Piece::X => write!(f, "X"),
+            Piece::O => write!(f, "O"),
+            Piece::Empty => write!(f, "_"),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum PlaceError {
     InvalidCoords,
     Occupied,
     Bad,
 }
 
-struct Board {
+pub struct Board {
     board: [[Piece; SIZE]; SIZE],
     turn: Piece,
 }
 
+impl Default for Board {
+    fn default() -> Self {
+        Board {
+            board: [[Piece::Empty; SIZE]; SIZE],
+            turn: Piece::X,
+        }
+    }
+}
+
 impl Board {
-    fn make_move(&mut self, x: usize, y: usize) -> std::result::Result<(), PlaceError> {
+    pub fn make_move(&mut self, x: usize, y: usize) -> std::result::Result<(), PlaceError> {
         if x >= SIZE || y >= SIZE {
             return Err(PlaceError::InvalidCoords);
         }
@@ -34,14 +62,98 @@ impl Board {
         Ok(()) 
     }
 
-    fn check_horizontal(self, row:usize) -> Piece {
-        let mut x_win: bool = true;
-        for i in 0..SIZE {
-            x_win = match self.board[row][i] {
-                Piece::O => false,
-                _ => true,
+    fn check_horizontal(&self, row: usize) -> Piece {
+        let mut x_win = true;
+        let mut o_win = true;
+        for &cell in &self.board[row] {
+            if cell != Piece::X {
+                x_win = false;
             }
+            if cell != Piece::O {
+                o_win = false;
+            }
+        }
+        if x_win {
+            Piece::X
+        } else if o_win {
+            Piece::O
+        } else {
+            Piece::Empty
         }
     }
 
+    fn check_vertical(&self, col:usize) -> Piece {
+        let mut x_win = true;
+        let mut o_win = true;
+        for i in 0..SIZE {
+            let &cell = &self.board[i][col];
+            if cell != Piece::X {
+                x_win = false;
+            }
+            if cell != Piece::O {
+                o_win = false;
+            }
+        }
+        if x_win {
+            Piece::X
+        } else if o_win {
+            Piece::O
+        } else {
+            Piece::Empty
+        }
+    }
+
+    fn check_diagonals(&self) -> Piece {
+        let mut x_win_l = true;
+        let mut x_win_r = true;
+        let mut o_win_l = true;
+        let mut o_win_r = true;
+        for i in 0..SIZE {
+            for j in 0..SIZE {
+                if self.board[i][j] != Piece::X {
+                    x_win_l = false;
+                }
+                if self.board[i][j] != Piece::O {
+                    o_win_l = false;
+                }
+                if self.board[(SIZE - 1) - i][(SIZE - 1) - j] != Piece::X {
+                    x_win_r = false;
+                }
+                if self.board[(SIZE - 1) - i][(SIZE - 1) - j] != Piece::O {
+                    o_win_r = false;
+                }
+            }
+        }
+
+        if x_win_l == true || x_win_r == true {
+            return Piece::X
+        } else if o_win_l == true || o_win_r == true {
+            return Piece::O
+        } else {
+            return Piece::Empty
+        }
+    }
+
+    pub fn check_win(&self) -> Piece {
+        for i in 0..SIZE {
+            match self.check_horizontal(i) {
+                Piece::Empty => {},
+                result => return result,
+            }
+            match self.check_vertical(i) {
+                Piece::Empty => {},
+                result => return result,
+            }
+        }
+        return self.check_diagonals();
+    }
+
+    pub fn print(&self) {
+        for i in 0..SIZE {
+            for j in 0..SIZE {
+                print!("{}", self.board[i][j]);
+            }
+            println!();
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,50 @@
+use std::io;
 mod board;
 
 fn main() {
-    println!("Hello, world!");
+    let mut b: board::Board = board::Board::default();
+
+    loop {
+        b.print();
+        match b.check_win() {
+            board::Piece::X => {
+                println!("X is the winner!");
+                break;
+            },
+            board::Piece::O => {
+                println!("O is the winner!");
+                break;
+            },
+            _ => {},
+        }
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read line");
+
+        let mut iter = input.split_whitespace();
+
+        if input.eq_ignore_ascii_case("q") {
+            break;
+        }
+
+        let result = match (iter.next(), iter.next()) {
+            (Some(num1_str), Some(num2_str)) => {
+                match (num1_str.parse::<usize>(), num2_str.parse::<usize>()) {
+                    (Ok(num1), Ok(num2)) => b.make_move(num1, num2),
+                    _ => {
+                        println!("Error parsing");
+                        Err(board::PlaceError::Bad)
+                    }
+                }
+            }
+            _ => {
+                eprintln!("Invalid input format. Please enter two integers separated by a space.");
+                Err(board::PlaceError::Bad)
+            }
+        };
+        match result {
+            Ok(()) => {},
+            Err(err) => eprintln!("Move failed: {:?}", err),
+        }
+    }
 }
